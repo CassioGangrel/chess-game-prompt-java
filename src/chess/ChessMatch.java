9b03fbcb336d6f2c73241f7ad26f2c8a3c251e7e
cassiogangrel <cassiomolusco@hotmail.com>
2018-12-20 08:48:51 -0200

    Colocando peças no board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardlayer.Board;
+import boardlayer.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -18,5 +22,11 @@ public class ChessMatch {
 
         }
         return match;
+    }
+
+    private void initialSetup() {
+        board.plaicePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.plaicePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.plaicePiece(new King(board, Color.WHITE), new Position(7, 4));
     }
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,20 +5,14 @@ import boardlayer.Piece;
 
 public class ChessPiece extends Piece {
     private Color color;
-    private int moveCount;
 
-    public ChessPiece(Board board, Color color, int moveCount) {
+    public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
-        this.moveCount = moveCount;
     }
 
     public Color getColor() {
         return color;
-    }
-
-    public int getMoveCount() {
-        return moveCount;
     }
 
     @Override
